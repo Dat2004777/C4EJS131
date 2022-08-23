4e8f93bcb9f8d0f0bb9438578dba71bc39420d11
DDJ <dat2004777@gmail.com>
2022-08-24 02:17:12 +0700

    Lession

--- Day-8-Mentor/script.js
+++ Day-8-Mentor/script.js
@@ -1,9 +1,26 @@
+// HW1
+
 let height = 5;
+let write = "";
+let result = "";
 
 for (let i = 1; i <= height; i++) {
-
-    for (let j = 1; j <= i; j++) {
-        console.log("*");
-    }
-    console.log("\n");
+    write += "*";
+    result += write + "\n";
 }
+
+console.log(result);
+
+// HW2
+
+let height2 = 5;
+let write2 = "*";
+let result2 = "";
+
+for (let i = 0; i < height2; i++) {
+    air = " ".repeat(height2 - i - 1);
+    result2 += air + write2 + "\n";
+    write2 += "**";
+}
+
+console.log(result2);
