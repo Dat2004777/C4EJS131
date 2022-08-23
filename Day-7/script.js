ae391913ee4b9cbe76d843cb0886cc318a7531ea
DDJ <dat2004777@gmail.com>
2022-08-24 00:58:44 +0700

    Lession

---
+++ Day-7/script.js
@@ -0,0 +1,22 @@
+// HW1
+let greeting = "Hello. My name is john";
+
+let result = "Chữ: " + greeting.charAt(8);
+
+console.log(result);
+
+// HW2
+let str1 = "The morning is upon us.";
+
+let result_2 = "Chuỗi mới: " + str1.slice(12);
+
+console.log(result_2);
+
+// HW3
+let str2 = "Hello";
+
+let str3 = " World";
+
+let result_3 = "Chuỗi mới: " + str2.concat(str3);
+
+console.log(result_3);
